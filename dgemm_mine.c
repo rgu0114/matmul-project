bb5413e0778cdefe5d32bf6d1ea2fec06e9253fe
Brian La <brianyla@gmail.com>
2024-09-23 19:14:09 +0000

    Loop ordering

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -5,7 +5,7 @@ const char* dgemm_desc = "My awesome dgemm.";
 // Tune this number by trying lots of different stuff
 #define BLOCK_SIZE 48
 
-#include <immintrin.h>  // Header for AVX intrinsics
+#include <immintrin.h> // Header for AVX intrinsics
 
 /* V5: SIMD */
 /** Performs mat_mul on a sub-block of the input matrices using SIMD */
@@ -52,9 +52,10 @@ const char* dgemm_desc = "My awesome dgemm.";
 // }
 
 /* V4: Copy optimization */
-void do_block(const int M, const int block_size, double *A_block, double *B_block, 
-              const double *A, const double *B, double *C, const int i_start, 
-              const int j_start, const int k_start) {
+void do_block(const int M, const int block_size, double* A_block, double* B_block,
+    const double* A, const double* B, double* C, const int i_start,
+    const int j_start, const int k_start)
+{
 
     memset(A_block, 0, block_size * block_size * sizeof(double));
     memset(B_block, 0, block_size * block_size * sizeof(double));
@@ -84,9 +85,10 @@ void do_block(const int M, const int block_size, double *A_block, double *B_bloc
     printf("done");
 }
 
-void square_dgemm(const int M, const double *A, const double *B, double *C) {
-    double *A_block = (double *) aligned_alloc(sizeof(double), BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
-    double *B_block = (double *) aligned_alloc(sizeof(double), BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
+void square_dgemm(const int M, const double* A, const double* B, double* C)
+{
+    double* A_block = (double*)aligned_alloc(sizeof(double), BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
+    double* B_block = (double*)aligned_alloc(sizeof(double), BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
 
     for (int i = 0; i < M; i += BLOCK_SIZE) {
         for (int j = 0; j < M; j += BLOCK_SIZE) {
@@ -107,24 +109,24 @@ void square_dgemm(const int M, const double *A, const double *B, double *C) {
 }
 
 /* V3: Loop orders */
-// /** Performs mat_mul on a sub-block of the input matrices */
-// void do_block(const int M, const int block_size, const double *A, const double *B, double *C,
-//               const int i_start, const int j_start, const int k_start) {    
+/** Performs mat_mul on a sub-block of the input matrices */
+// void do_block(const int M, const int block_size, const double* A, const double* B, double* C,
+//     const int i_start, const int j_start, const int k_start)
+// {
 //     for (int j = j_start; j < j_start + block_size && j < M; ++j) {
-//         for (int i = i_start; i < i_start + block_size && i < M; ++i) {
-//             double cij = C[j * M + i];
-//             for (int k = k_start; k < k_start + block_size && k < M; ++k) {
-//                 cij += A[k * M + i] * B[j * M + k];
+//         for (int k = k_start; k < k_start + block_size && k < M; ++k) {
+//             for (int i = i_start; i < i_start + block_size && i < M; ++i) {
+//                 C[j * M + i] += A[k * M + i] * B[j * M + k];
 //             }
-//             C[j * M + i] = cij;
 //         }
 //     }
 // }
 
-// void square_dgemm(const int M, const double *A, const double *B, double *C) {
-//     for (int i = 0; i < M; i += BLOCK_SIZE) {
-//         for (int j = 0; j < M; j += BLOCK_SIZE) {
-//             for (int k = 0; k < M; k += BLOCK_SIZE) {
+// void square_dgemm(const int M, const double* A, const double* B, double* C)
+// {
+//     for (int j = 0; j < M; j += BLOCK_SIZE) {
+//         for (int k = 0; k < M; k += BLOCK_SIZE) {
+//             for (int i = 0; i < M; i += BLOCK_SIZE) {
 //                 do_block(M, BLOCK_SIZE, A, B, C, i, j, k);
 //             }
 //         }
@@ -156,7 +158,6 @@ void square_dgemm(const int M, const double *A, const double *B, double *C) {
 //         }
 //     }
 // }
-
 
 /* V1: Naive Implementation */
 // void square_dgemm(const int M, const double *A, const double *B, double *C)
